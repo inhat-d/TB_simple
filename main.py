eb08a79bdb1d0040417b095bfa6efd5d9d1b0395
tiny_mauss <105206479+inhat-d@users.noreply.github.com>
2023-12-11 13:49:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#maded by 7's grade child
+
+import telebot
+
+#about tokens you can read in "YouMayDontReadThis"
+bot = telebot.TeleBot("6693417645:AAHPeop5jUs15cGCtrp2qr0Ah7tgOGXE_T8")
+
+#first command, /start in tg chat
+@bot.message_handler(commands=["start"])    # IMPORTANT, write commands without slash
+def start(message):
+	bot.send_message(message.chat.id, f"Hello {message.from_user.username}, i am a bot answer example")
+
+#second command, /help in tg chat
+@bot.message_handler(commands=["help", "commands"])   # you can assign functions to multiple commands
+def help(message):
+	bot.send_message(message.chat.id, f"@{message.from_user.username}, here's my commands")
+  bot.send_message(message.chat.id, "/start - hello command")
+  bot.send_message(message.chat.id, "/help - this command")    #also you can write many messages
+
+#there are many other teams, you can learn about them on YouTube 
+
+bot.polling(none_stop=True)
+#this command exists to prevent the project from terminating
